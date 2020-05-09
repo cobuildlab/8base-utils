e2b845be5c993c046937a8f518169ae8c2a0c91c
Angel Lacret <alacret@gmail.com>
2020-05-09 19:25:17 -0400

    eslint and formatting

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {ValidationError} from "./error/ValidationError";
+import { ValidationError } from './error/ValidationError';
 
 /**
  * Helper to change non null keys to 8base 'connect' reference.
@@ -7,30 +7,44 @@ import {ValidationError} from "./error/ValidationError";
  * @param {object} data - The Object to be Mutated.
  * @param {string} key - The key in the Object.
  */
-export const normalize8BaseConnect = (data: Record<string, any>, key: string) => {
-    if (key === "" || key === null) {
-        throw new ValidationError(`normalize8BaseConnect: key: can't be blank or null.`);
-    }
+export const normalize8BaseConnect = (
+  data: Record<string, any>,
+  key: string,
+) => {
+  if (key === '' || key === null) {
+    throw new ValidationError(
+      `normalize8BaseConnect: key: can't be blank or null.`,
+    );
+  }
 
-    const currentValue = data[key];
-    if (currentValue === null || currentValue === "") {
-        delete data[key];
-        return;
-    }
+  const currentValue = data[key];
+  if (currentValue === null || currentValue === '') {
+    delete data[key];
+    return;
+  }
 
-    if (typeof currentValue === "string") {
-        data[key] = {connect: {id: currentValue}};
-        return;
-    }
+  if (typeof currentValue === 'string') {
+    data[key] = { connect: { id: currentValue } };
+    return;
+  }
 
-    if (typeof currentValue === "object") {
-        const currentId = currentValue.id;
-        if (currentId === null || currentId === undefined || typeof currentId !== "string" || currentId === "") {
-            throw new ValidationError(`normalize8BaseConnect: the value on data of the key: '${key}' is not a string, instead is: '${currentId}'`)
-        }
-        data[key] = {connect: {id: currentId}};
-        return;
+  if (typeof currentValue === 'object') {
+    const currentId = currentValue.id;
+    if (
+      currentId === null ||
+      currentId === undefined ||
+      typeof currentId !== 'string' ||
+      currentId === ''
+    ) {
+      throw new ValidationError(
+        `normalize8BaseConnect: the value on data of the key: '${key}' is not a string, instead is: '${currentId}'`,
+      );
     }
+    data[key] = { connect: { id: currentId } };
+    return;
+  }
 
-    throw new ValidationError(`normalize8BaseConnect: '${key}' in data is not a 'string' or a 'object'.`);
+  throw new ValidationError(
+    `normalize8BaseConnect: '${key}' in data is not a 'string' or a 'object'.`,
+  );
 };
