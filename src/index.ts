7f17167317f388aa2839bc7d8df9587123223cf1
Luis Eulacio <luiseulacio1255@gmail.com>
2020-06-04 16:38:24 -0400

    Export validation function to index

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from "./files";
 export * from "./references";
 export {_validateFile} from "./utils";
 export {_validateFiles} from "./utils";
+export {_validateNullOrUndefinedOrBlank} from "./utils";
+
